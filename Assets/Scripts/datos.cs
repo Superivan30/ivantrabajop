fbd411b434e448f69eebf13a8cf97d34a546304d
Techtegno <techtegno@gmail.com>
2024-02-20 08:23:19 -0500

    JAJA

--- Assets/Scripts/datos.cs
+++ Assets/Scripts/datos.cs
@@ -17,6 +17,10 @@ public class DataJSON : System.Object
     public int salud;
     [SerializeField]
     public string correo;
+    [SerializeField]
+    public Vector3 playerLocation;
+
+    
 
     public DataJSON()
     {
